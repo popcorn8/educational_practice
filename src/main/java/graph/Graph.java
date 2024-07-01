4548c33b8d601da6aa46fc36923961182d075ac0
trixeran <danil854458@gmail.com>
2024-07-01 14:50:35 +0300

    add delete func, loadGraph, saveGraph
    добавлено удаление, сохранение графа и выгрузка графа

--- src/main/java/graph/Graph.java
+++ src/main/java/graph/Graph.java
@@ -2,6 +2,7 @@ package graph;
 
 import java.awt.*;
 import java.awt.geom.Line2D;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -9,6 +10,7 @@ import java.util.List;
 public class Graph {
     private List<Node> nodes;
     private List<Edge> edges;
+    private static final int NODE_PROXIMITY_RADIUS = 25;
 
     public Graph() {
         nodes = new ArrayList<>();
@@ -30,8 +32,8 @@ public class Graph {
         }
     }
 
-    public void addEdge(Node start, Node end, Color color) {
-        edges.add(new Edge(start, end, color));
+    public void addEdge(Node start, Node end, Color color, int thickness) {
+        edges.add(new Edge(start, end, color, thickness));
     }
 
     public void removeEdge(Edge edge) {
@@ -40,7 +42,7 @@ public class Graph {
 
     public Node findNode(int x, int y) {
         for (Node node : nodes) {
-            if (node.contains(x, y)) {
+            if (node.contains(x, y) || isNearNode(node, x, y)) {
                 return node;
             }
         }
@@ -58,9 +60,75 @@ public class Graph {
         return null;
     }
 
+    public void clearGraph() {
+        nodes.clear();
+        edges.clear();
+        //TODO: не забыть добавить repaint(); при нажатии удаления.
+
+    }
+
     private boolean isPointNearLine(int x, int y, int x1, int y1, int x2, int y2) {
         double distance = Line2D.ptSegDist(x1, y1, x2, y2, x, y);
         return distance < 5.0;
+    }
+
+    private boolean isNearNode(Node node, int x, int y) {
+        return node.getPoint().distance(x, y) <= NODE_PROXIMITY_RADIUS;
+    }
+
+    public void loadGraph(String filename) {
+        clearGraph();  // Clear the existing graph
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            String line;
+            List<Node> fileNodes = new ArrayList<>();
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(" ");
+                if (parts[0].equals("N")) {
+                    // Node line
+                    int x = Integer.parseInt(parts[1]);
+                    int y = Integer.parseInt(parts[2]);
+                    Node node = new Node(x, y);
+                    addNode(x, y);
+                    fileNodes.add(node);
+                } else if (parts[0].equals("E")) {
+                    // Edge line
+                    int startIdx = Integer.parseInt(parts[1]);
+                    int endIdx = Integer.parseInt(parts[2]);
+                    int r = Integer.parseInt(parts[3]);
+                    int g = Integer.parseInt(parts[4]);
+                    int b = Integer.parseInt(parts[5]);
+                    int thickness = Integer.parseInt(parts[6]);
+                    Color color = new Color(r, g, b);
+
+                    if (startIdx < fileNodes.size() && endIdx < fileNodes.size()) {
+                        addEdge(fileNodes.get(startIdx), fileNodes.get(endIdx), color, thickness);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void saveGraph(String filename) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(filename))) {
+            for (int i = 0; i < nodes.size(); i++) {
+                Node node = nodes.get(i);
+                bw.write(String.format("N %d %d\n", node.getPoint().x, node.getPoint().y));
+            }
+            for (int i = 0; i < edges.size(); i++) {
+                Edge edge = edges.get(i);
+                int startIdx = nodes.indexOf(edge.getStart());
+                int endIdx = nodes.indexOf(edge.getEnd());
+                int r = edge.getColor().getRed();
+                int g = edge.getColor().getGreen();
+                int b = edge.getColor().getBlue();
+                bw.write("E" + " " + startIdx + " " + endIdx + " " + r + " " + g + " " + b + " " + edge.getThickness() + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public List<Node> getNodes() {
@@ -110,15 +178,17 @@ public class Graph {
         private Node start;
         private Node end;
         private Color color;
+        private int thickness;
 
         public Edge(Node start, Node end) {
-            this(start, end, Color.BLACK);
+            this(start, end, Color.BLACK, 3);
         }
 
-        public Edge(Node start, Node end, Color color) {
+        public Edge(Node start, Node end, Color color, int thickness) {
             this.start = start;
             this.end = end;
             this.color = color;
+            this.thickness = thickness;
         }
 
         public Node getStart() {
@@ -135,6 +205,14 @@ public class Graph {
 
         public void setColor(Color color) {
             this.color = color;
+        }
+
+        public int getThickness() {
+            return thickness;
+        }
+
+        public void setThickness(int thickness) {
+            this.thickness = thickness;
         }
     }
 }
--- src/main/java/graph/GraphPanel.java
+++ src/main/java/graph/GraphPanel.java
@@ -1,169 +1,3 @@
-//package graph;
-//
-//import javax.swing.*;
-//import java.awt.*;
-//import java.awt.event.MouseAdapter;
-//import java.awt.event.MouseEvent;
-//import java.awt.event.MouseMotionAdapter;
-//import java.awt.geom.Line2D;
-//import java.util.ArrayList;
-//import java.util.Iterator;
-//import java.util.List;
-//
-//public class GraphPanel extends JPanel {
-//    private List<Node> nodes;
-//    private List<Edge> edges;
-//    private Node selectedNode;
-//    private int edgeThickness = 3;
-//
-//    public GraphPanel() {
-//        nodes = new ArrayList<>();
-//        edges = new ArrayList<>();
-//        selectedNode = null;
-//
-//        addMouseListener(new MouseAdapter() {
-//            @Override
-//            public void mousePressed(MouseEvent e) {
-//                if (SwingUtilities.isLeftMouseButton(e)) {
-//                    Node node = findNode(e.getX(), e.getY());
-//                    if (node == null) {
-//                        nodes.add(new Node(e.getX(), e.getY()));
-//                    } else {
-//                        selectedNode = node;
-//                        selectedNode.setColor(Color.RED); // Установить цвет вершины в красный
-//                        setEdgesColor(selectedNode, Color.RED); // Установить цвет рёбер в красный
-//                    }
-//                    repaint();
-//                } else if (SwingUtilities.isRightMouseButton(e)) {
-//                    Node node = findNode(e.getX(), e.getY());
-//                    if (node != null) {
-//                        removeNode(node);
-//                    } else {
-//                        Edge edge = findEdge(e.getX(), e.getY());
-//                        if (edge != null) {
-//                            removeEdge(edge);
-//                        }
-//                    }
-//                    repaint();
-//                } else if (SwingUtilities.isMiddleMouseButton(e)) {
-//                    Node node = findNode(e.getX(), e.getY());
-//                    if (node != null) {
-//                        selectedNode = node;
-//                        selectedNode.setColor(Color.RED); // Установить цвет вершины в красный при выборе
-//                        setEdgesColor(selectedNode, Color.RED); // Установить цвет рёбер в красный при выборе
-//                        repaint();
-//                    }
-//                }
-//            }
-//
-//            @Override
-//            public void mouseReleased(MouseEvent e) {
-//                if (SwingUtilities.isLeftMouseButton(e)) {
-//                    if (selectedNode != null) {
-//                        Node node = findNode(e.getX(), e.getY());
-//                        if (node != null && node != selectedNode) {
-//                            edges.add(new Edge(selectedNode, node, Color.RED)); // Установить цвет ребра в красный
-//                        }
-//                        selectedNode.setColor(Color.BLACK); // Вернуть цвет вершины в чёрный
-//                        setEdgesColor(selectedNode, Color.BLACK); // Вернуть цвет рёбер в чёрный
-//                        selectedNode = null;
-//                    }
-//                    repaint();
-//                } else if (SwingUtilities.isMiddleMouseButton(e)) {
-//                    if (selectedNode != null) {
-//                        selectedNode.setColor(Color.BLACK); // Вернуть цвет вершины в чёрный
-//                        setEdgesColor(selectedNode, Color.BLACK); // Вернуть цвет рёбер в чёрный
-//                        selectedNode = null;
-//                    }
-//                    repaint();
-//                }
-//            }
-//        });
-//
-//        addMouseMotionListener(new MouseMotionAdapter() {
-//            @Override
-//            public void mouseDragged(MouseEvent e) {
-//                if (SwingUtilities.isMiddleMouseButton(e) && selectedNode != null) {
-//                    selectedNode.setPoint(e.getX(), e.getY());
-//                    selectedNode.setColor(Color.RED); // Установить цвет вершины в красный при перетаскивании
-//                    setEdgesColor(selectedNode, Color.RED); // Установить цвет рёбер в красный при перетаскивании
-//                    repaint();
-//                }
-//            }
-//        });
-//    }
-//
-//    private Node findNode(int x, int y) {
-//        for (Node node : nodes) {
-//            if (node.contains(x, y)) {
-//                return node;
-//            }
-//        }
-//        return null;
-//    }
-//
-//    private Edge findEdge(int x, int y) {
-//        for (Edge edge : edges) {
-//            Point p1 = edge.getStart().getPoint();
-//            Point p2 = edge.getEnd().getPoint();
-//            if (isPointNearLine(x, y, p1.x, p1.y, p2.x, p2.y)) {
-//                return edge;
-//            }
-//        }
-//        return null;
-//    }
-//
-//    private boolean isPointNearLine(int x, int y, int x1, int y1, int x2, int y2) {
-//        double distance = Line2D.ptSegDist(x1, y1, x2, y2, x, y);
-//        return distance < 5.0;
-//    }
-//
-//    private void removeNode(Node node) {
-//        nodes.remove(node);
-//        Iterator<Edge> iterator = edges.iterator();
-//        while (iterator.hasNext()) {
-//            Edge edge = iterator.next();
-//            if (edge.getStart() == node || edge.getEnd() == node) {
-//                iterator.remove();
-//            }
-//        }
-//    }
-//
-//    private void removeEdge(Edge edge) {
-//        edges.remove(edge);
-//    }
-//
-//    private void setEdgesColor(Node node, Color color) {
-//        for (Edge edge : edges) {
-//            if (edge.getStart() == node || edge.getEnd() == node) {
-//                edge.setColor(color);
-//            }
-//        }
-//    }
-//
-//    @Override
-//    protected void paintComponent(Graphics g) {
-//        super.paintComponent(g);
-//
-//        Graphics2D g2d = (Graphics2D) g;
-//        g2d.setStroke(new BasicStroke(edgeThickness));
-//
-//        for (Edge edge : edges) {
-//            g2d.setColor(edge.getColor());
-//            g2d.drawLine(edge.getStart().getPoint().x, edge.getStart().getPoint().y,
-//                    edge.getEnd().getPoint().x, edge.getEnd().getPoint().y);
-//        }
-//
-//        for (Node node : nodes) {
-//            g2d.setColor(node.getColor()); // Использование цвета вершины
-//            g2d.fillOval(node.getPoint().x - node.getRadius(), node.getPoint().y - node.getRadius(),
-//                    2 * node.getRadius(), 2 * node.getRadius());
-//            g2d.drawOval(node.getPoint().x - node.getRadius(), node.getPoint().y - node.getRadius(),
-//                    2 * node.getRadius(), 2 * node.getRadius());
-//        }
-//    }
-//}
-
 package graph;
 
 import javax.swing.*;
@@ -175,10 +9,10 @@ import java.awt.event.MouseMotionAdapter;
 public class GraphPanel extends JPanel {
     private Graph graph;
     private Graph.Node selectedNode;
-    private int edgeThickness = 3;
 
     public GraphPanel() {
-        graph = new Graph();
+
+        this.graph = new Graph();
         selectedNode = null;
 
         addMouseListener(new MouseAdapter() {
@@ -188,6 +22,10 @@ public class GraphPanel extends JPanel {
                     Graph.Node node = graph.findNode(e.getX(), e.getY());
                     if (node == null) {
                         graph.addNode(e.getX(), e.getY());
+//                        System.out.println(graph.getNodes().getFirst().getPoint().getX() + " " + graph.getNodes().getFirst().getPoint().getY());
+//                        graph.loadGraph("src/main/resources/save_graph.txt");
+//                        graph.saveGraph("src/main/resources/save_graph.txt");
+                        repaint();
                     } else {
                         selectedNode = node;
                         selectedNode.setColor(Color.RED);
@@ -222,7 +60,7 @@ public class GraphPanel extends JPanel {
                     if (selectedNode != null) {
                         Graph.Node node = graph.findNode(e.getX(), e.getY());
                         if (node != null && node != selectedNode) {
-                            graph.addEdge(selectedNode, node, Color.RED);
+                            graph.addEdge(selectedNode, node, Color.RED, 3);
                         }
                         selectedNode.setColor(Color.BLACK);
                         setEdgesColor(selectedNode, Color.BLACK);
@@ -266,10 +104,10 @@ public class GraphPanel extends JPanel {
         super.paintComponent(g);
 
         Graphics2D g2d = (Graphics2D) g;
-        g2d.setStroke(new BasicStroke(edgeThickness));
 
         for (Graph.Edge edge : graph.getEdges()) {
             g2d.setColor(edge.getColor());
+            g2d.setStroke(new BasicStroke(edge.getThickness()));
             g2d.drawLine(edge.getStart().getPoint().x, edge.getStart().getPoint().y,
                     edge.getEnd().getPoint().x, edge.getEnd().getPoint().y);
         }
@@ -283,4 +121,3 @@ public class GraphPanel extends JPanel {
         }
     }
 }
-
